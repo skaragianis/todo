56d5f393b455678632be18aa102317dea3b85ebc
Steffan Karagianis <steffan.karagianis@gmail.com>
2025-11-07 15:33:57 +1100

    Started the parser for todos

---
+++ internal/todo/parser.go
@@ -0,0 +1,74 @@
+package todo
+
+import (
+	"errors"
+	"time"
+)
+
+type Todo struct {
+	description string
+	completed   bool
+	completedAt time.Time
+	createdAt   time.Time
+}
+
+func Parse(line string) (*Todo, error) {
+	todo := &Todo{}
+
+	if line == "" {
+		return todo, nil
+	}
+
+	pos := 0
+
+	if line[pos] == 'x' {
+		todo.completed = true
+		pos += 2
+	}
+
+	if pos >= len(line) {
+		return nil, errors.New("you marked it completed but there's no detail for what?")
+	}
+
+	var date1, date2 time.Time
+	var err error
+	if line[pos] >= '0' && line[pos] <= '9' {
+		date1, err = time.Parse("2006-01-02", line[pos:pos+10])
+		if err != nil {
+			return nil, errors.New("invalid date")
+		}
+
+		pos += 11
+	}
+
+	if pos >= len(line) {
+		return nil, errors.New("you specified a date but then no detail for what?")
+	}
+
+	if line[pos] >= '0' && line[pos] <= '9' {
+		// Assume a date
+		date2, err = time.Parse("2006-01-02", line[pos:pos+10])
+		if err != nil {
+			return nil, errors.New("invalid date")
+		}
+
+		pos += 11
+	}
+
+	if pos >= len(line) {
+		return nil, errors.New("you specified a creation date but no detail supporting why?")
+	}
+
+	if todo.completed {
+		todo.completedAt = date1
+		todo.createdAt = date2
+
+	} else {
+		todo.createdAt = date1
+	}
+
+	// The rest is the description
+	todo.description = line[pos:]
+
+	return todo, nil
+}
---
+++ internal/todo/parser_test.go
@@ -0,0 +1,48 @@
+package todo
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func mustDate(s string) time.Time {
+	t, err := time.Parse("2006-01-02", s)
+	if err != nil {
+		panic(err)
+	}
+
+	return t
+}
+
+func TestParse(t *testing.T) {
+	tests := []struct {
+		in        string
+		todo      Todo
+		expectErr bool
+	}{
+		{"a simple todo", Todo{"a simple todo", false, time.Time{}, time.Time{}}, false},
+		{"A simple todo", Todo{"A simple todo", false, time.Time{}, time.Time{}}, false},
+		{"x A simple todo", Todo{"A simple todo", true, time.Time{}, time.Time{}}, false},
+		{"X A simple todo", Todo{"X A simple todo", false, time.Time{}, time.Time{}}, false},
+		{"2025-02-01 A simple todo", Todo{"A simple todo", false, time.Time{}, mustDate("2025-02-01")}, false},
+		{"x 2025-02-01 A simple todo", Todo{"A simple todo", true, mustDate("2025-02-01"), time.Time{}}, false},
+		{"x 2025-02-01 2025-06-10 A simple todo", Todo{"A simple todo", true, mustDate("2025-02-01"), mustDate("2025-06-10")}, false},
+		{"2025-02-01 A simple todo", Todo{"A simple todo", false, time.Time{}, mustDate("2025-02-01")}, false},
+
+		// {"x 2016-05-20 2-16-04-30 a full example of a todo containing @context and a +tag plus some text on the end", Todo{}, false},
+	}
+
+	for _, test := range tests {
+		result, err := Parse(test.in)
+		if !test.expectErr && err == nil {
+			if !reflect.DeepEqual(test.todo, *result) {
+				t.Errorf("Parse(%s): Wanted: %v Got: %v", test.in, test.todo, *result)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("Parse(%s): Wanted: %v Got: %v", test.in, test.expectErr, err)
+			}
+		}
+	}
+}
