9aedc1c8bbed44567ab0299810722e10482ae7e4
Steffan Karagianis <steffan.karagianis@gmail.com>
2025-11-08 11:39:01 +1100

    Added context, projects and tags

--- internal/todo/parser.go
+++ internal/todo/parser.go
@@ -2,6 +2,7 @@ package todo
 
 import (
 	"errors"
+	"strings"
 	"time"
 )
 
@@ -11,12 +12,19 @@ type Todo struct {
 	priority    byte
 	completedAt time.Time
 	createdAt   time.Time
+	projects    []string
+	contexts    []string
+	tags        map[string]string
 }
 
 const DateFormat = "2006-01-02"
 
 func Parse(line string) (*Todo, error) {
-	todo := &Todo{}
+	todo := &Todo{
+		projects: []string{},
+		contexts: []string{},
+		tags:     map[string]string{},
+	}
 
 	if line == "" {
 		return todo, nil
@@ -83,6 +91,17 @@ func Parse(line string) (*Todo, error) {
 	}
 
 	todo.description = line[pos:]
+
+	words := strings.Fields(line)
+	for _, word := range words {
+		if strings.HasPrefix(word, "+") {
+			todo.projects = append(todo.projects, word[1:])
+		} else if strings.HasPrefix(word, "@") {
+			todo.contexts = append(todo.contexts, word[1:])
+		} else if parts := strings.Split(word, ":"); len(parts) == 2 && parts[0] != "" {
+			todo.tags[parts[0]] = parts[1]
+		}
+	}
 
 	return todo, nil
 }
